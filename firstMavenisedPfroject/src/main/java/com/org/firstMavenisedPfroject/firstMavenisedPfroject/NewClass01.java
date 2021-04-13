0d1dc92ecd318b2a3231cd746521e22d28299796
USER <USER@DESKTOP-U4FNCL1.Dlink>
2021-04-13 16:16:56 +0200

    This is my first commit to master branch of this project

---
+++ firstMavenisedPfroject/src/main/java/com/org/firstMavenisedPfroject/firstMavenisedPfroject/NewClass01.java
@@ -0,0 +1,11 @@
+package com.org.firstMavenisedPfroject.firstMavenisedPfroject;
+
+public class NewClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
